e8584ceee515927bc2ebfe6b5c96fffddfec8ed9
Jaredible <jareddiehl96@gmail.com>
2020-03-16 22:14:31 -0500

    Updated

--- src/main/Benchmark.java
+++ src/main/Benchmark.java
@@ -1,9 +1,18 @@
 package main;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.function.Consumer;
 
 public class Benchmark {
+
+  /** A 1-megabyte preallocation to ensure the heap is reasonably sized. */
+  public static byte[] memoryReserve = new byte[1024 * 1024 * 1];
 
   private static final int INTEGER_MIN = 1;
   private static final int INTEGER_MAX = 10000;
@@ -57,24 +66,93 @@ public class Benchmark {
     return array;
   };
 
-  private static void benchmark1(ITestDataGenerator testDataGenerator) {
-    for (int i = 1000; i <= 100000; i *= 10) {
-      System.out.println(i);
-      int[] array = testDataGenerator.generate(i);
+  private static long doSortTest(String arrayType, Tuple<String, Consumer<int[]>> sortingFunctionTuple, Tuple<Integer, List<int[]>> arrayTuple, int testCount) throws IOException {
+    String filename = String.format("%s_%s_%d.csv", arrayType, String.join("_", sortingFunctionTuple.x.toLowerCase().replace(" sort", "").split(" ")), arrayTuple.x);
+    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filename)));
+    writer.write("Time");
 
-      long selectionSort = time(SelectionSort::sort, array);
-      long insertionSort = time(InsertionSort::sort, array);
-      long bubbleSortA = time(BubbleSort::sortA, array);
-      long bubbleSortB = time(BubbleSort::sortB, array);
-      long quickSort = time(QuickSort::sort, array);
-      long mergeSort = time(MergeSort::sort, array);
+    long timeSum = 0;
 
-      System.out.println(String.format("%.4f seconds", mergeSort / 1e9d));
-    }
+    int i = 0;
+    do {
+      writer.write("\n");
+      long time = time(sortingFunctionTuple.y, arrayTuple.y.get(i));
+      System.out.println(String.format("%.4f", time / 1e9d));
+      writer.write(String.format("%.4f", time / 1e9d));
+      timeSum += time;
+    } while (++i < testCount);
+
+    writer.close();
+
+    return timeSum;
   }
 
-  public static void main(String[] args) {
-    benchmark1(randomTestData);
+  private static void benchmark(ITestDataGenerator testDataGenerator, String arrayType, int[] arraySizes, int testCount) throws IOException {
+    String filename = String.format("benchmark_%s_output.csv", arrayType);
+    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filename)));
+    writer.write("Array Size, Selection Sort, Insertion Sort, Bubble Sort A, Bubble Sort B, Quick Sort, Merge Sort\n");
+
+    List<Tuple<String, Consumer<int[]>>> sortingFunctionTuples = new ArrayList<Tuple<String, Consumer<int[]>>>();
+    sortingFunctionTuples.add(new Tuple<String, Consumer<int[]>>("Selection Sort", SelectionSort::sort));
+    sortingFunctionTuples.add(new Tuple<String, Consumer<int[]>>("Insertion Sort", InsertionSort::sort));
+    sortingFunctionTuples.add(new Tuple<String, Consumer<int[]>>("Bubble Sort A", BubbleSort::sortA));
+    sortingFunctionTuples.add(new Tuple<String, Consumer<int[]>>("Bubble Sort B", BubbleSort::sortB));
+    sortingFunctionTuples.add(new Tuple<String, Consumer<int[]>>("Quick Sort", QuickSort::sort));
+    sortingFunctionTuples.add(new Tuple<String, Consumer<int[]>>("Merge Sort", MergeSort::sort));
+
+    List<Tuple<Integer, List<int[]>>> arrayTuples = new ArrayList<Tuple<Integer, List<int[]>>>();
+
+    for (Integer arraySize : arraySizes) {
+      Tuple<Integer, List<int[]>> arrayTuple = new Tuple<Integer, List<int[]>>(arraySize, new ArrayList<int[]>());
+      for (int j = 0; j < testCount; j++) {
+        arrayTuple.y.add(testDataGenerator.generate(arraySize));
+      }
+      arrayTuples.add(arrayTuple);
+    }
+
+    for (Tuple<Integer, List<int[]>> arrayTuple : arrayTuples) {
+      System.out.println(arrayTuple.x);
+      writer.write(String.format("%d,", arrayTuple.x));
+
+      for (Tuple<String, Consumer<int[]>> sortingFunctionTuple : sortingFunctionTuples) {
+        System.out.println(sortingFunctionTuple.x);
+
+        long timeSum = doSortTest(arrayType, sortingFunctionTuple, arrayTuple, testCount);
+        double timeAverage = timeSum / testCount;
+        System.out.println(String.format("%.4f", timeAverage / 1e9d));
+        writer.write(String.format("%.4f,", timeAverage / 1e9d));
+      }
+
+      System.out.flush();
+      writer.write("\n");
+    }
+
+    writer.close();
+  }
+
+  public static void main(String[] args) throws IOException {
+    int[] arraySizes = {1000, 10000, 100000};
+
+    System.out.println("Benchmarking random test data");
+    benchmark(randomTestData, "random", arraySizes, 10);
+
+    System.out.println("Benchmarking sorted test data");
+    benchmark(sortedTestData, "sorted", arraySizes, 10);
+
+    System.out.println("Benchmarking almost-sorted test data");
+    benchmark(almostSortedTestData, "almost_sorted", arraySizes, 10);
+  }
+
+  private static class Tuple<X, Y> {
+
+    public final X x;
+    public final Y y;
+
+    public Tuple(X x, Y y) {
+      this.x = x;
+      this.y = y;
+    }
+
   }
 
 }
